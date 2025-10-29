eb53158688347b5b0073df5904d7fb7872ff1c63
Brian Flores <brian.flores.ch@gmail.com>
2025-10-28 23:01:59 -0300

    add: install frontend

---
+++ apps/frontend/pos-elli/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold underline font-montserrat">
+        Hello world!
+      </h1>
+      <p className="font-inter text-gray-700">
+        Este texto usa Inter.
+      </p>
+    </div>
+  )
+}
+
+export default App
